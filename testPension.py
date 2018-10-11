77b94251845d9175c1abf9cf203ea2f9da3187dc
Moisés González <moisesgonzalez@outlook.com>
2018-10-11 13:54:19 -0400

    Agregar primera prueba, falla porque no se ha definido la función.

--- testPension.py
+++ testPension.py
@@ -1,10 +1,14 @@
 import unittest
 from datetime import date
+from classPension import Pension
 
 class Test(unittest.TestCase):
 
-    def test_pass(self):
-        pass
+    def test_hombre_60_750sem_0InS(self):
+        hoy = date.today()
+        sesenta = hoy.replace(year=hoy.year-60)
+        pensionado = Pension(sesenta, 'M', 750, 0)
+        self.assertTrue( pensionado.cumple() )
 
 
 if __name__ == "__main__":
