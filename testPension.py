124ae0cb615b7f29f31153950ea65d61c7f30093
MoisesGonzalezS <moisesgonzalez@outlook.com>
2018-10-11 17:21:43 -0400

    Agregar caso de prueba para reducción de 5 años.
    Agregar caso de prueba de un hombre y una mujer con 20 años trabajando
    en condiciones insalubres y a 5 años de cumplir la edad requerida. La
    prueba falla porque no se considera la reduccion de la edad límite por
    condiciones de trabajo.

--- testPension.py
+++ testPension.py
@@ -4,13 +4,13 @@ from classPension import Pension
 
 class Test(unittest.TestCase):
 
-    def test_hombre_60_750sem_0InS(self):
+    def test_hombre60_750sem_0InS(self):
         hoy = date.today()
         nacimiento = hoy.replace(year=hoy.year-60)
         pensionado = Pension(nacimiento, 'M', 750, 0)
         self.assertTrue( pensionado.cumple() )
 
-    def test_hombre_59_750sem_0Ins(self):
+    def test_hombre59_750sem_0Ins(self):
         hoy = date.today()
         nacimiento = hoy.replace(year=hoy.year-60, day=hoy.day+1)
         pensionado = Pension(nacimiento, 'M', 750, 0)
@@ -51,6 +51,18 @@ class Test(unittest.TestCase):
         nacimiento = hoy.replace(year=hoy.year-55, day=hoy.day+1)
         pensionado = Pension(nacimiento, 'F', 749, 0)
         self.assertFalse( pensionado.cumple() )
+
+    def test_hombre55_750_sem_20Ins(self):
+        hoy = date.today()
+        nacimiento = hoy.replace(year=hoy.year-55)
+        pensionado = Pension(nacimiento, 'M', 750, 20)
+        self.assertTrue( pensionado.cumple() )
+
+    def test_mujer50_750_sem_20Ins(self):
+        hoy = date.today()
+        nacimiento = hoy.replace(year=hoy.year-50)
+        pensionado = Pension(nacimiento, 'F', 750, 20)
+        self.assertTrue( pensionado.cumple() )
 
 if __name__ == "__main__":
     unittest.main()
