1258d45da2030dfb345c0c5d4a74d9702255717d
Moisés González <moisesgonzalez@outlook.com>
2018-10-11 14:41:33 -0400

    Agregar código a la función. Primera prueba pasa.

--- classPension.py
+++ classPension.py
@@ -14,6 +14,15 @@ class Pension:
         self.semAcotizar = 750
         self.edadObjetivo = 60
 
+
     def cumple(self):
-        pass
+        return (self.calcularEdad() >= self.edadObjetivo and
+                 self.semCotizadas >= self.semAcotizar)
+
+    def calcularEdad(self):
+        hoy = date.today()
+        nac = self.fechaNac
+        edad = hoy.year - nac.year - (
+                (hoy.month, hoy.day) < (nac.month, nac.day) )
+        return edad
 
