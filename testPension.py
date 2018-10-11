9cdb788bf74bced73970c0aca8f8fd25e92850bb
Moisés González <moisesgonzalez@outlook.com>
2018-10-11 15:06:09 -0400

    Agregar nuevo caso de prueba y renombrar variables.
    Se agrega el caso de una mujer con 750 semanas cotizadas, 0 años en
    condiciones insalubres y 55 años de edad. La prueba falla porque no se
    toma en cuenta la edad mínima para mujeres.

--- testPension.py
+++ testPension.py
@@ -6,27 +6,33 @@ class Test(unittest.TestCase):
 
     def test_hombre_60_750sem_0InS(self):
         hoy = date.today()
-        sesenta = hoy.replace(year=hoy.year-60)
-        pensionado = Pension(sesenta, 'M', 750, 0)
+        nacimiento = hoy.replace(year=hoy.year-60)
+        pensionado = Pension(nacimiento, 'M', 750, 0)
         self.assertTrue( pensionado.cumple() )
 
     def test_hombre_59_750sem_0Ins(self):
         hoy = date.today()
-        sesenta = hoy.replace(year=hoy.year-60, day=hoy.day+1)
-        pensionado = Pension(sesenta, 'M', 750, 0)
+        nacimiento = hoy.replace(year=hoy.year-60, day=hoy.day+1)
+        pensionado = Pension(nacimiento, 'M', 750, 0)
         self.assertFalse( pensionado.cumple() )
 
     def test_hombre60_749_sem_0Ins(self):
         hoy = date.today()
-        sesenta = hoy.replace(year=hoy.year-60)
-        pensionado = Pension(sesenta, 'M', 749, 0)
+        nacimiento = hoy.replace(year=hoy.year-60)
+        pensionado = Pension(nacimiento, 'M', 749, 0)
         self.assertFalse( pensionado.cumple() )
 
     def test_hombre59_749_sem_0Ins(self):
         hoy = date.today()
-        sesenta = hoy.replace(year=hoy.year-60, day=hoy.day+1)
-        pensionado = Pension(sesenta, 'M', 749, 0)
+        nacimiento = hoy.replace(year=hoy.year-60, day=hoy.day+1)
+        pensionado = Pension(nacimiento, 'M', 749, 0)
         self.assertFalse( pensionado.cumple() )
+
+    def test_mujer55_750_sem_0Ins(self):
+        hoy = date.today()
+        nacimiento = hoy.replace(year=hoy.year-55)
+        pensionado = Pension(nacimiento, 'F', 750, 0)
+        self.assertTrue( pensionado.cumple() )
 
 if __name__ == "__main__":
     unittest.main()
