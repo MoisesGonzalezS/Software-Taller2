c49857603a06438986d1042afb4150d75f4077f7
Moisés González <moisesgonzalez@outlook.com>
2018-10-11 14:56:33 -0400

    Agregar más pruebas
    Añadir los casos de 749 semanas cotizadas para un hombre con 60 años y
    con 59 a un día de cumplir los 60, ambos con 0 años de trabajo en condiciones
    insalubres. Todas las pruebas pasan.

--- testPension.py
+++ testPension.py
@@ -10,12 +10,23 @@ class Test(unittest.TestCase):
         pensionado = Pension(sesenta, 'M', 750, 0)
         self.assertTrue( pensionado.cumple() )
 
-    def test_hombre_59_750sem_oIns(self):
+    def test_hombre_59_750sem_0Ins(self):
         hoy = date.today()
         sesenta = hoy.replace(year=hoy.year-60, day=hoy.day+1)
         pensionado = Pension(sesenta, 'M', 750, 0)
         self.assertFalse( pensionado.cumple() )
 
+    def test_hombre60_749_sem_0Ins(self):
+        hoy = date.today()
+        sesenta = hoy.replace(year=hoy.year-60)
+        pensionado = Pension(sesenta, 'M', 749, 0)
+        self.assertFalse( pensionado.cumple() )
+
+    def test_hombre59_749_sem_0Ins(self):
+        hoy = date.today()
+        sesenta = hoy.replace(year=hoy.year-60, day=hoy.day+1)
+        pensionado = Pension(sesenta, 'M', 749, 0)
+        self.assertFalse( pensionado.cumple() )
 
 if __name__ == "__main__":
     unittest.main()
